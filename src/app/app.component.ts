c801159c0b20c30d7a7e8e8d417f8c57f655c2af
Kostas Christou <christou@exeo.gr>
2023-06-26 11:30:05 +0300

    before refactoring data service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'wiki-search';
+  pages = [];
+
+  constructor(private wikiService: WikipediaService) {}
+
+  onTerm(term: string) {
+    this.wikiService.search(term).subscribe((response: any) => {
+      console.log(response.query.search);
+      this.pages = response.query.search;
+    });
+  }
 }
+
+// https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=space
